46e7189bb31415fa14294a6d0b4f99ecd6dff1cb
Angel Vanegas <angelman@gmail.com>
2016-07-25 15:33:07 -0400

    initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+exp:
+	./sfront -orc experi.saol -sco experi.sasl -aout sounds.wav
+sa:
+	gcc sa.c -o stuff
---
+++ score_gen.rb
@@ -0,0 +1,26 @@
+# time-start name(const) time-dur pitch(0-127)
+# "0 tone 0 0"
+# "0.5 end"
+
+def ending num
+  "#{num+1} end"
+end
+
+def score start, instr
+  dur = rand + rand(0.15..0.6)
+  pch = rand(55..73)
+  "#{start} #{instr} #{dur.round 1} #{pch}"
+end
+
+def repeat amount
+  arr = []
+  next_start = 0.25
+  amount.times do |f|
+    arr << score(next_start, "tone")
+    next_start += arr[f].split[2].to_f
+    next_start = next_start.round 2
+  end
+  arr << ending(next_start)
+end
+
+puts repeat 20
